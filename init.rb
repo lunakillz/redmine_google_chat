05e091123ec4d20bff45bb97d2f5b9c5fe8d0f4f
Logan <logan@breathings.co.kr>
2020-07-16 20:09:36 +0900

    update init

--- init.rb
+++ init.rb
@@ -1,11 +1,12 @@
-require 'redmine'
+require 'redmine_google_chat'
 
 Redmine::Plugin.register :redmine_google_chat do
 	name 'Redmine Google Chat plugin'
 	author 'Changho Song'
-	url 'https://github.com/lunakillz/redmine-google-chat'
+	url 'https://github.com/lunakillz/redmine_google_chat'
 	description 'A Redmine plugin posts Google Chat on creating and updating tickets'
 	version '0.1'
+	permission :manage_hook, {:webhook_settings => [:index, :show, :update, :create, :destroy]}, :require => :member
 	requires_redmine :version_or_higher => '4.0.0'
 
 	settings \
@@ -14,3 +15,4 @@ Redmine::Plugin.register :redmine_google_chat do
 		},
 		:partial => 'settings/google_chat_settings'
 end
+l
