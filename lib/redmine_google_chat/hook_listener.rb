a973fcd6a0e87d548007329785124944100e757c
Logan <logan@breathings.co.kr>
2020-07-20 10:32:13 +0900

    update Card

--- lib/redmine_google_chat/hook_listener.rb
+++ lib/redmine_google_chat/hook_listener.rb
@@ -35,18 +35,11 @@ module RedmineGoogleChat
 	
 		private
 		def issue_to_json(issue, controller)
-			msg = {
-				:project_name => issue.project,
-				:author => issue.author.to_s,
-				:action => "created",
-				:issue => issue,
-				:link => controller.issue_url(issue),
-				:mentions => "#{mentions issue.description}"
-			}
+			text = "#{issue.author.to_s} created #{escape issue}"
 			card = {}
 			card[:header] = {
-				:title => "#{msg[:author]} #{msg[:action]} #{escape msg[:issue]} #{msg[:mentions]}",
-				:subtitle => "#{escape msg[:project_name]}"
+				:title => "#{escape issue.project,}",
+				:subtitle => "#{mentions issue.description}"
 			}
 			widgets = [{
 				:keyValue => {
@@ -67,6 +60,7 @@ module RedmineGoogleChat
 					:topLabel => I18n.t("field_assigned_to"),
 					:content => escape(issue.assigned_to.to_s),
 					:contentMultiline => "false"
+					:icon => "PERSON"
 				}
 			} if issue.assigned_to
 
@@ -93,6 +87,7 @@ module RedmineGoogleChat
 			]
 	
 			{
+				:text => text,
 				:cards => card
 			}.to_json
 		end
@@ -118,68 +113,7 @@ module RedmineGoogleChat
 			end
 		end
 	
-		def detail_to_field(detail)
-			if detail.property == "cf"
-				key = CustomField.find(detail.prop_key).name rescue nil
-				title = key
-			elsif detail.property == "attachment"
-				key = "attachment"
-				title = I18n.t :label_attachment
-			else
-				key = detail.prop_key.to_s.sub("_id", "")
-				if key == "parent"
-					title = I18n.t "field_#{key}_issue"
-				else
-					title = I18n.t "field_#{key}"
-				end
-			end
-	
-			short = true
-			value = escape detail.value.to_s
-	
-			case key
-			when "title", "subject", "description"
-				short = false
-			when "tracker"
-				tracker = Tracker.find(detail.value) rescue nil
-				value = escape tracker.to_s
-			when "project"
-				project = Project.find(detail.value) rescue nil
-				value = escape project.to_s
-			when "status"
-				status = IssueStatus.find(detail.value) rescue nil
-				value = escape status.to_s
-			when "priority"
-				priority = IssuePriority.find(detail.value) rescue nil
-				value = escape priority.to_s
-			when "category"
-				category = IssueCategory.find(detail.value) rescue nil
-				value = escape category.to_s
-			when "assigned_to"
-				user = User.find(detail.value) rescue nil
-				value = escape user.to_s
-			when "fixed_version"
-				version = Version.find(detail.value) rescue nil
-				value = escape version.to_s
-			when "attachment"
-				attachment = Attachment.find(detail.prop_key) rescue nil
-				value = "<#{object_url attachment}|#{escape attachment.filename}>" if attachment
-			when "parent"
-				issue = Issue.find(detail.value) rescue nil
-				value = "<#{object_url issue}|#{escape issue}>" if issue
-			end
-	
-			value = "-" if value.empty?
-	
-			result = { 
-				:keyValue => { 
-					:topLabel => title,
-					:content => value 
-				} 
-			} 
-			result[:keyValue][:contentMultiline] = "true" if not short
-			result
-		end
+		
 	
 		def mentions text
 			return nil if text.nil?
